cfd716c523b0cfa8f3c89ea6e84dc44f3e66a04d
wendellmoraisz <wendellmorais3319@gmail.com>
2024-01-27 16:16:06 -0300

    feat: get user by id

--- src/services/firebaseService.ts
+++ src/services/firebaseService.ts
@@ -1,6 +1,14 @@
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { query, collection, where, getDocs } from "firebase/firestore";
+import { db } from "../config/firebaseConfig";
 
 export async function login(email: string, password: string) {
     const auth = getAuth();
     return await signInWithEmailAndPassword(auth, email, password);
+}
+
+export async function getUserById(id: string) {
+    const q = query(collection(db, "users"), where("id", "==", id));
+    const response = await getDocs(q);
+    return response.docs[0].data();
 }
