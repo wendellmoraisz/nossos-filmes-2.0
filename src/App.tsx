da4826b66baf56735d2bc33c4c7875a22afa6511
wendellmoraisz <wendellmorais3319@gmail.com>
2024-01-28 17:40:00 -0300

    refactor: move index logic to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,57 @@
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login.tsx";
+import { createTheme, ThemeProvider } from "@mui/material";
+import AuthProvider from "./context/AuthProvider.tsx";
+import { Toaster } from "react-hot-toast";
+import Home from "./pages/home/Home.tsx";
+
+const defautTheme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#242A32"
+    },
+    primary: {
+      main: "#32E0C4"
+    },
+    secondary: {
+      main: "#242A32"
+    }
+  },
+  typography: {
+    fontFamily: "Poppins"
+  }
+});
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />
+  },
+  {
+    path: "/home",
+    element: <Home />
+  }
+]);
 
 function App() {
 
   return (
-    <>
-      <h1>ola</h1>
-    </>
+    <ThemeProvider theme={defautTheme}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+      <Toaster
+        toastOptions={{
+          duration: 2000,
+          style: {
+            background: "#242A32",
+            color: "#FFF",
+            boxShadow: "1px 5px 14px 1px rgba(0, 0, 0, 0.2)"
+          }
+        }}
+      />
+    </ThemeProvider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,53 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Login from "./pages/Login.tsx";
-import { createTheme, ThemeProvider } from "@mui/material";
-import AuthProvider from "./context/AuthProvider.tsx";
-import { Toaster } from "react-hot-toast";
-
-const defautTheme = createTheme({
-  palette: {
-    mode: "dark",
-    background: {
-      default: "#242A32"
-    },
-    primary: {
-      main: "#32E0C4"
-    },
-    secondary: {
-      main: "#242A32"
-    }
-  },
-  typography: {
-    fontFamily: "Poppins"
-  }
-});
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />
-  }
-])
+import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={defautTheme}>
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
-      <Toaster
-        toastOptions={{
-          duration: 2000,
-          style: {
-            background: "#242A32",
-            color: "#FFF",
-            boxShadow: "1px 5px 14px 1px rgba(0, 0, 0, 0.2)"
-          }
-        }}
-      />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
 )
