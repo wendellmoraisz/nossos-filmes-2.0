4e4f4042ae2f36d07a22b119a7a08dba4ae37e2f
wendellmoraisz <wendellmorais3319@gmail.com>
2024-01-29 14:19:16 -0300

    raw: add evaluations constant

---
+++ src/data/constants/evaluations.ts
@@ -0,0 +1,24 @@
+const evaluations = [
+    {
+        value: 1,
+        description: "Roubos de Halloween 🎃"
+    },
+    {
+        value: 2,
+        description: "Tatuagem do Holt 🐶"
+    },
+    {
+        value: 3,
+        description: "Máquina de doces 🍬"
+    },
+    {
+        value: 4,
+        description: "Comissária Want 🧹"
+    },
+    {
+        value: 5,
+        description: "Pai do Peralta ☠"
+    },
+];
+
+export default evaluations;
