f703832ec2f0fb4f03cce31eaacaa7bd6daa0b1b
wendellmoraisz <wendellmorais3319@gmail.com>
2024-02-04 09:44:25 -0300

    feat: add support for displaying minutes and hours in the converted time

--- src/utils/convertMinutesToHours.ts
+++ src/utils/convertMinutesToHours.ts
@@ -1,5 +1,10 @@
 export function convertMinutesToHours(minutes: number) {
+  if (minutes < 60) return `${minutes}min`;
+
   const hours = Math.floor(minutes / 60);
   const remainingMinutes = minutes % 60;
+
+  if (remainingMinutes === 0) return `${hours}h`;
+  
   return `${hours}h ${remainingMinutes}min`;
 }
