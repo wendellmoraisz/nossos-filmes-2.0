10e7af23f0d153875b042440bec144c68c223cdd
wendellmoraisz <wendellmorais3319@gmail.com>
2024-05-04 16:03:41 -0300

    feat: show second digit of budget and revenue

--- src/utils/abbreviateNumber.ts
+++ src/utils/abbreviateNumber.ts
@@ -1,12 +1,17 @@
 export function abbreviateNumber(value: number) {
-    if (value < 1000) {
+    if (value < 1000)
         return value;
+
+    if (value >= 1000 && value < 1000000)
+        return abbreviation(1000, "K");
+
+    if (value >= 1000000 && value < 1000000000)
+        return abbreviation(1000000, "M");
+
+    return abbreviation(1000000000, "B");
+
+    function abbreviation(divisor: number, suffix: string) {
+        const toFixedFraction = value % divisor === 0 ? 0 : 1;
+        return `${(value / divisor).toFixed(toFixedFraction)}${suffix}`;
     }
-    if (value >= 1000 && value < 1000000) {
-        return `${(value / 1000).toFixed(0)}K`;
-    }
-    if (value >= 1000000 && value < 1000000000) {
-        return `${(value / 1000000).toFixed(0)}M`;
-    }
-    return `${(value / 1000000000).toFixed(0)}B`;
 }
