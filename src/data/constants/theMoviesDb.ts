957248bf662ca6350817f96da54a0ecc56b7dcc4
Wendell <wendellmorais.dev@gmail.com>
2026-08-01 20:07:25 -0300

    perf: use optimized TMDB image size (w500) to improve load times

--- src/data/constants/theMoviesDb.ts
+++ src/data/constants/theMoviesDb.ts
@@ -1,2 +1,2 @@
-export const MOVIE_POSTER_BASE_URL = `https://image.tmdb.org/t/p/original`;
+export const MOVIE_POSTER_BASE_URL = `https://image.tmdb.org/t/p/w500`;
 export const BASE_URL = `https://api.themoviedb.org/3`;
